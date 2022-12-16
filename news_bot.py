8027d92a07abd93c36e103ddf25da7b6495fb88d
joshstubble <62226937+joshstubble@users.noreply.github.com>
2022-12-16 16:45:47 -0500

    Update news_bot.py

--- news_bot.py
+++ news_bot.py
@@ -15,15 +15,16 @@ client = discord.Client(intents=intents)
 API_KEY = os.environ["GOOGLE_NEWS_API_KEY"]
 CHANNEL_ID = os.environ["DISCORD_CHANNEL_ID"]
 
+
 @client.event
 async def on_ready():
     # Send a starting message to the "news" channel
     news_channel = discord.utils.get(client.get_all_channels(), id=int(CHANNEL_ID))
-    await news_channel.send("News bot starting up! I'll be posting news articles every minute.")
+    await news_channel.send("News bot starting up! I'll be posting news articles every ten minutes.")
     # Start a timer to retrieve news articles every hour
     printed = False  # Flag to track whether the response has been printed
     while True:
-        await asyncio.sleep(60)
+        await asyncio.sleep(600)
         # Build the query string for the Google News API
         query = "site:ft.com OR site:politico.com OR site:bloomberg.com OR site:wsj.com OR site:apnews.com OR site:reuters.com OR site:nytimes.com OR site:foxnews.com OR site:aljazeera.com when:1h"
         params = {
