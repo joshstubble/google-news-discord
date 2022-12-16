08ba77a9fb52b5ecead60b01db0b6d74df7d27a6
joshstubble <62226937+joshstubble@users.noreply.github.com>
2022-12-16 00:48:08 -0500

    Create google.py

---
+++ google.py
@@ -0,0 +1,31 @@
+import asyncio
+import discord
+import requests
+
+client = discord.Client()
+
+# Set the API key for the Google News API
+API_KEY = "YOUR_API_KEY_HERE"
+
+@client.event
+async def on_ready():
+    # Start a timer to retrieve news articles every minute
+    while True:
+        await asyncio.sleep(60)
+        # Build the query string for the Google News API
+        query = "site:ft.com OR site:politico.com OR site:bloomberg.com OR site:wsj.com OR site:apnews.com OR site:reuters.com OR site:nytimes.com OR site:foxnews.com OR site:aljazeera.com when:1h"
+        params = {
+            "q": query,
+            "sortBy": "publishedAt",
+            "apiKey": API_KEY
+        }
+        # Make the API request
+        response = requests.get("https://newsapi.org/v2/everything", params=params)
+        # Parse the response and retrieve the articles
+        articles = response.json()["articles"]
+        # Send a message with the articles to the "news" channel
+        news_channel = discord.utils.get(client.get_all_channels(), id=CHANNEL_ID_HERE)
+        for article in articles:
+            await news_channel.send(f"{article['title']}\n{article['url']}")
+
+client.run("YOUR_BOT_TOKEN_HERE")
