d84ab5589292c5578133cbe4b611237d493d6322
joshstubble <62226937+joshstubble@users.noreply.github.com>
2022-12-16 16:04:51 -0500

    Update news_bot.py

--- news_bot.py
+++ news_bot.py
@@ -17,9 +17,12 @@ CHANNEL_ID = os.environ["DISCORD_CHANNEL_ID"]
 
 @client.event
 async def on_ready():
-    # Start a timer to retrieve news articles every minute
+    # Send a starting message to the "news" channel
+    news_channel = discord.utils.get(client.get_all_channels(), id=int(CHANNEL_ID))
+    await news_channel.send("News bot starting up! I'll be posting news articles every ten minutes.")
+    # Start a timer to retrieve news articles every hour
     while True:
-        await asyncio.sleep(60)
+        await asyncio.sleep(600)
         # Build the query string for the Google News API
         query = "site:ft.com OR site:politico.com OR site:bloomberg.com OR site:wsj.com OR site:apnews.com OR site:reuters.com OR site:nytimes.com OR site:foxnews.com OR site:aljazeera.com when:1h"
         params = {
@@ -40,7 +43,6 @@ async def on_ready():
             logger.error("Error parsing API response: %s", e)
             continue
         # Send a message with the articles to the "news" channel
-        news_channel = discord.utils.get(client.get_all_channels(), id=int(CHANNEL_ID))
         for article in articles:
             await news_channel.send(f"{article['title']}\n{article['url']}")
 
